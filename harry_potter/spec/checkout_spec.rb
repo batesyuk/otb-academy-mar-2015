7b88a5ff485a4d76857a2922e5e131881bab954d
James Walsh <james.walsh@onthebeach.co.uk>
2015-03-05 18:55:11 +0000

    Added all tests the the checkout spec

---
+++ harry_potter/spec/checkout_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe "This checkout has" do
+
+  it "one book without discount" do
+    items = [1]
+    expect(basket_count(items)).to eq(8)
+    # 1*8
+  end
+
+  it "two different books with a 5% discount" do
+    items = [1, 1]
+    expect(basket_count(items)).to eq(15.20)
+    # 2*8*.95
+  end
+
+  it "three different books with a 10% discount" do
+    items = [1, 1, 1]
+    expect(basket_count(items)).to eq(21.6)
+    # 3*8*.9
+  end
+
+  it "four different books with a 20% discount" do
+    items = [1, 1, 1, 1]
+    expect(basket_count(items)).to eq(25.6)
+    # 4*8*.8
+  end
+
+  it "five different books with a 25% discount" do
+    items = [1, 1, 1, 1, 1]
+    expect(basket_count(items)).to eq(30)
+    # 5*8*.75
+  end
+
+  it "two of the same book without discount" do
+    items = [2]
+    expect(basket_count(items)).to eq(16)
+    # 2*8
+  end
+
+  it "three books with a 5% discount" do
+    items = [2, 1]
+    expect(basket_count(items)).to eq(23.2)
+    # 2*8*.95+1*8
+  end
+
+  it "two of the same book without discount" do
+    items = [2]
+    expect(basket_count(items)).to eq(51.2)
+    # 4*8*.8+4*8*.8
+  end
+end
