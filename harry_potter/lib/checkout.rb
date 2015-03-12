4183de2e5bdae663eda790cfea42bda8b73cc262
James Walsh <james.walsh@onthebeach.co.uk>
2015-03-12 13:11:03 +0000

    Change layout of items array and integers and added sets

--- harry_potter/lib/checkout.rb
+++ harry_potter/lib/checkout.rb
@@ -1,25 +1,85 @@
-def basket_count(items)
-  count = 0
-  price = 8
+class Checkout
 
-  total = Array.new
-  while count < items.length do
-    calc = items[count] * 8
+  attr_reader :items, :total
+  RRP = 8
 
-    if items.length == 5
-      total << calc * 0.75
-    elsif items.length == 4
-      total << calc * 0.8
-    elsif items.length == 3
-      total << calc * 0.9
-    elsif items.length == 2
-      total << calc * 0.95
-    else
-      total << calc
-    end
-    count += 1
+  def initialize(items)
+    @items = items
   end
 
-  total.reduce(:+)
+  def handle_total
+    calculate_basket
+  end
+
+  def calculate_basket
+      create_sets
+      basket_count
+  end
+
+  def create_sets
+    @sets = Array.new
+
+    while @items.length > 0
+      parts = @items.uniq
+      @sets << parts
+      parts.each { |x| @items.delete_at(@items.index(x)) }
+    end
+
+    @sets
+    p @sets
+  end
+
+  def basket_count
+
+    @total = Array.new
+
+    if @sets.length == 2
+
+      @first = Array.new
+      @last = Array.new
+
+      if @sets.first.length == 5
+        @first << @sets.first.length * RRP * 0.75
+      elsif @sets.first.length == 4
+        @first << @sets.first.length * RRP * 0.8
+      elsif @sets.first.length == 3
+        @first << @sets.first.length * RRP * 0.9
+      elsif @sets.first.length == 2
+        @first << @sets.first.length * RRP * 0.95
+      else
+        @first << @sets.first.length * RRP
+      end
+
+      if @sets.last.length == 5
+        @last << @sets.last.length * RRP * 0.75
+      elsif @sets.last.length == 4
+        @last << @sets.last.length * RRP * 0.8
+      elsif @sets.last.length == 3
+        @last << @sets.last.length * RRP * 0.9
+      elsif @sets.last.length == 2
+        @last << @sets.last.length * RRP * 0.95
+      else
+        @last << @sets.last.length * RRP
+      end
+
+      @first = @first.reduce(:+)
+      @last = @last.reduce(:+)
+
+      @total << @first + @last
+
+    elsif @sets.first.length == 5
+      @total << @sets.first.length * RRP * 0.75
+    elsif @sets.first.length == 4
+      @total << @sets.first.length * RRP * 0.8
+    elsif @sets.first.length == 3
+      @total << @sets.first.length * RRP * 0.9
+    elsif @sets.first.length == 2
+      @total << @sets.first.length * RRP * 0.95
+    else
+      @total << @sets.first.length * RRP
+    end
+
+    @total = @total.reduce(:+)
+  end
 
 end
--- harry_potter/spec/checkout_spec.rb
+++ harry_potter/spec/checkout_spec.rb
@@ -4,49 +4,65 @@ RSpec.describe "This checkout has" do
 
   it "one book without discount" do
     items = [1]
-    expect(basket_count(items)).to eq(8)
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(8)
     # 1*8
   end
 
   it "two different books with a 5% discount" do
-    items = [1, 1]
-    expect(basket_count(items)).to eq(15.20)
+    items = [1, 2]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(15.20)
     # 2*8*.95
   end
 
   it "three different books with a 10% discount" do
-    items = [1, 1, 1]
-    expect(basket_count(items)).to eq(21.6)
+    items = [1, 2, 3]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(21.6)
     # 3*8*.9
   end
 
   it "four different books with a 20% discount" do
-    items = [1, 1, 1, 1]
-    expect(basket_count(items)).to eq(25.6)
+    items = [1, 2, 3, 4]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(25.6)
     # 4*8*.8
   end
 
   it "five different books with a 25% discount" do
-    items = [1, 1, 1, 1, 1]
-    expect(basket_count(items)).to eq(30)
+    items = [1, 2, 3, 4, 5]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(30)
     # 5*8*.75
   end
 
   it "two of the same book without discount" do
-    items = [2]
-    expect(basket_count(items)).to eq(16)
+    items = [1, 1]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(16)
     # 2*8
   end
 
   it "three books with a 5% discount" do
-    items = [2, 1]
-    expect(basket_count(items)).to eq(23.2)
+    items = [1, 2, 1]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(23.2)
     # 2*8*.95+1*8
   end
 
   it "Max discount and multiple trilogy" do
-    items = [2, 2, 2, 1, 1  ]
-    expect(basket_count(items)).to eq(51.2)
+    items = [1, 2, 3, 4, 5, 1, 2, 3]
+    checkout = Checkout.new(items)
+
+    expect(checkout.handle_total).to eq(51.2)
     # 4*8*.8+4*8*.8
   end
 end
