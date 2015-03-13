f5912e8417c05eff6d91f18e78cf26eeae310e44
James Walsh <james.walsh@onthebeach.co.uk>
2015-03-13 09:46:51 +0000

    refactored basket calculations

--- harry_potter/lib/checkout.rb
+++ harry_potter/lib/checkout.rb
@@ -11,6 +11,18 @@ class Checkout
     calculate_basket
   end
 
+  def handle_discount
+    {
+      1 => 1,
+      2 => 0.95,
+      3 => 0.9,
+      4 => 0.8,
+      5 => 0.75
+    }
+  end
+
+
+
   def calculate_basket
       create_sets
       basket_count
@@ -38,45 +50,16 @@ class Checkout
       @first = Array.new
       @last = Array.new
 
-      if @sets.first.length == 5
-        @first << @sets.first.length * RRP * 0.75
-      elsif @sets.first.length == 4
-        @first << @sets.first.length * RRP * 0.8
-      elsif @sets.first.length == 3
-        @first << @sets.first.length * RRP * 0.9
-      elsif @sets.first.length == 2
-        @first << @sets.first.length * RRP * 0.95
-      else
-        @first << @sets.first.length * RRP
-      end
-
-      if @sets.last.length == 5
-        @last << @sets.last.length * RRP * 0.75
-      elsif @sets.last.length == 4
-        @last << @sets.last.length * RRP * 0.8
-      elsif @sets.last.length == 3
-        @last << @sets.last.length * RRP * 0.9
-      elsif @sets.last.length == 2
-        @last << @sets.last.length * RRP * 0.95
-      else
-        @last << @sets.last.length * RRP
-      end
+      @first << @sets.first.length * RRP * handle_discount[@sets.first.length]
+      @last << @sets.last.length * RRP * handle_discount[@sets.last.length]
 
       @first = @first.reduce(:+)
       @last = @last.reduce(:+)
 
       @total << @first + @last
 
-    elsif @sets.first.length == 5
-      @total << @sets.first.length * RRP * 0.75
-    elsif @sets.first.length == 4
-      @total << @sets.first.length * RRP * 0.8
-    elsif @sets.first.length == 3
-      @total << @sets.first.length * RRP * 0.9
-    elsif @sets.first.length == 2
-      @total << @sets.first.length * RRP * 0.95
     else
-      @total << @sets.first.length * RRP
+      @total << @sets.first.length * RRP * handle_discount[@sets.first.length]
     end
 
     @total = @total.reduce(:+)
