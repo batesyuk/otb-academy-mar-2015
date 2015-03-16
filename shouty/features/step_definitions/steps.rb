e4909408cb6d340529f0d9d14b23a3756705654e
James Walsh <james.walsh@onthebeach.co.uk>
2015-03-16 16:41:19 +0000

    James now hears the message

--- shouty/features/step_definitions/steps.rb
+++ shouty/features/step_definitions/steps.rb
@@ -1,21 +1,49 @@
-class Person
-  attr_writer :location
-
-  def shout(message)
+class Network
+  def initialize
+    @people = []
   end
 
-  def heard_messages
-    []
+  def add_person(person)
+    @people << person
+  end
+
+  def broadcast(message)
+    @people.each do |person|
+      person.hear_message(message)
+    end
   end
 end
 
+class Person
+  attr_writer :location
+  attr_reader :heard_messages
+  attr_accessor :network
+
+  def initialize(network)
+    network.add_person self
+    @heard_messages = Array.new
+    @network = network
+  end
+
+  def shout(message)
+    network.broadcast message
+  end
+
+  def hear_message(message)
+    @heard_messages << message
+  end
+
+end
+
 Given(/^James is at "(.*?)"$/) do |location|
-  @james = Person.new
+  @network ||= Network.new
+  @james = Person.new(@network)
   @james.location = location
 end
 
 Given(/^Chris is at "(.*?)"$/) do |location|
-  @chris = Person.new
+  @network ||= Network.new
+  @chris = Person.new(@network)
   @chris.location = location
 end
 
